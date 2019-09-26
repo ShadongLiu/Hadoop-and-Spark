d24814b41142dfee5685a98d40058bc5c00d26e4
Shadong Liu <shadongliu@gmail.com>
2019-09-26 15:37:30 -0400

    a2 fir

--- src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyPairs-1.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyPairs-1.scala
@@ -14,7 +14,7 @@
   * limitations under the License.
   */
 
-package io.bespin.scala.spark.bigram
+package ca.uwaterloo.cs451.a2
 
 import io.bespin.scala.util.Tokenizer
 
@@ -23,6 +23,7 @@ import org.apache.hadoop.fs._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
+import org.apache.spark.Partitioner
 
 class PairsConf(args: Seq[String]) extends ScallopConf(args) {
   mainOptions = Seq(input, output, reducers)
@@ -32,7 +33,15 @@ class PairsConf(args: Seq[String]) extends ScallopConf(args) {
   verify()
 }
 
-object BigramCount extends Tokenizer {
+class MyPartitioner(partitions: Int) extends Partitioner {
+  def numPartitions: Int = partitions
+  def getPartition(key: Any): Int = key match {
+    case null => 0
+    case(k1, k2) => (k1.hashCode & Integer.MAX_VALUE) % partitions
+  }
+}
+
+object ComputeBigramRelativeFrequecyPairs extends Tokenizer {
   val log = Logger.getLogger(getClass().getName())
 
   def main(argv: Array[String]) {
@@ -42,20 +51,38 @@ object BigramCount extends Tokenizer {
     log.info("Output: " + args.output())
     log.info("Number of reducers: " + args.reducers())
 
-    val conf = new SparkConf().setAppName("Bigram Count")
+    val conf = new SparkConf().setAppName("ComputeBigramRelativeFrequecyPairs")
     val sc = new SparkContext(conf)
 
     val outputDir = new Path(args.output())
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
     val textFile = sc.textFile(args.input())
+    var marginal = 0.0
     val counts = textFile
       .flatMap(line => {
         val tokens = tokenize(line)
-        if (tokens.length > 1) tokens.sliding(2).map(p => p.mkString(" ")).toList else List()
+        if (tokens.length > 1) {
+          val pairs = tokens.sliding(2).map(p => (p.head.mkString, p.tail.mkString)).toList
+          val wordmarginal = tokens.init.map(p => (p, "*"))
+          //concatenation of list of pairs and list of marginal values
+          pairs ++ wordmarginal
+        }else List()
       })
-      .map(bigram => (bigram, 1))
+      .map(bigram => (bigram, 1.0f))
+      //(x,y) => x + y
       .reduceByKey(_ + _)
+      .sortByKey().partitionBy(new MyPartitioner(args.reducers()))
+      .map(bigram => bigram._1 match{
+        case (_,"*") => {
+          marginal = bigram._2
+          (bigram._1, bigram._2)
+        }
+        case (_,_) => {
+          (bigram._1, bigram._2/marginal)
+        }
+      })
+      .map(p => "(" + p._1._1 + ", " + p._1._2 + "), " + p._2)
     counts.saveAsTextFile(args.output())
   }
 }
