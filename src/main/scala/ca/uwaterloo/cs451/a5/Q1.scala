ee5bc35b181df780bd989dddb70a10603047ac6d
Shadong Liu <shadongliu@gmail.com>
2019-11-08 21:23:46 -0500

    a5 first

---
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
@@ -0,0 +1,70 @@
+/**
+  * Bespin: reference implementations of "big data" algorithms
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package ca.uwaterloo.cs451.a5
+
+import io.bespin.scala.util.Tokenizer
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import org.apache.spark.Partitioner
+import org.apache.spark.sql.SparkSession
+
+class Conf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, date, text, parquet)
+  val input = opt[String](descr = "input path", required = true)
+  val date = opt[String](descr = "date", required = false)
+  val text = opt[Int](descr = "text", required = false)
+  val parquet = opt[Int](descr = "parquet", required = false)
+  verify()
+}
+
+object Q1 {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new StripesConf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Date: " + args.date())
+
+    val conf = new SparkConf().setAppName("Q1")
+    val sc = new SparkContext(conf)
+    val date = args.date()
+
+    if (args.text()) {
+      val textFile = sc.textFile(args.input() + "/lineitem.tbl")
+      val count = textFile
+        .map(line => line.split("\\|")(10))
+        .filter(_.contains(date))
+        .count
+      println("ANSWER=" + count)
+    } else if (args.parquet()) {
+                 val sparkSession = SparkSession.builder.getOrCreate
+
+                 val lineitemDF =
+                   sparkSession.read.parquet(args.input() + "/lineitem")
+                 val lineitemRDD = lineitemDF.rdd
+                 val count = lineitemRDD
+                   .map(line => line.getString(10))
+                   .filter(_.contains(date))
+                   .count
+                 println("ANSWER=" + count)
+               }
+  }
+}
