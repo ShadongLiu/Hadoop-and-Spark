cddbf40e829b1df82bf3a2b6222759998677dbc5
Shadong Liu <shadongliu@gmail.com>
2019-11-09 19:41:40 -0500

    a5 thrid

---
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q3.scala
@@ -0,0 +1,104 @@
+/**
+  * Bespin: reference implementations of "big data" algorithms
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package ca.uwaterloo.cs451.a5
+
+import io.bespin.scala.util.Tokenizer
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import org.apache.spark.Partitioner
+import org.apache.spark.sql.SparkSession
+
+object Q3 {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Date: " + args.date())
+
+    val conf = new SparkConf().setAppName("Q3")
+    val sc = new SparkContext(conf)
+    val date = args.date()
+
+    if (args.text()) {
+      val part = sc
+        .textFile(args.input() + "/part.tbl")
+        .map(line => (line.split("\\|")(0).toInt, line.split("\\|")(1)))
+        .collectAsMap()
+      val pBroadcast = sc.broadcast(part)
+      val supplier = sc
+        .textFile(args.input() + "/supplier.tbl")
+        .map(line => (line.split("\\|")(0).toInt, line.split("\\|")(1)))
+        .collectAsMap()
+      val sBroadcast = sc.broadcast(supplier)
+      val lineitem = sc
+        .textFile(args.input() + "/lineitem.tbl")
+        .filter(line => line.split("\\|")(10).contains(date))
+        .map(line => {
+          val lines = line.split("\\|")
+          val orderKey = lines(0).toInt
+          val partKey = lines(1).toInt
+          val suppKey = lines(2).toInt
+          val pArray = pBroadcast.value
+          val sArray = sBroadcast.value
+          (orderKey, (pArray(partKey), sArray(suppKey)))
+        })
+        .sortByKey()
+        .take(20)
+        .foreach(p => println(p._1, p._2._1, p._2._2))
+    } else if (args.parquet()) {
+      val sparkSession = SparkSession.builder.getOrCreate
+      val partDF =
+        sparkSession.read.parquet(args.input() + "/part")
+      val partRDD = partDF.rdd
+      val part = partRDD
+        .map(line => (line.getInt(0), line.getString(1)))
+        .collectAsMap
+      val pBroadcast = sc.broadcast(part)
+
+      val supplierDF =
+        sparkSession.read.parquet(args.input() + "/supplier")
+      val supplierRDD = supplierDF.rdd
+      val supplier = supplierRDD
+        .map(line => (line.getInt(0), line.getString(1)))
+        .collectAsMap
+      val sBroadcast = sc.broadcast(supplier)
+
+      val lineitemDF =
+        sparkSession.read.parquet(args.input() + "/lineitem")
+      val lineitemRDD = lineitemDF.rdd
+      val lineitem = lineitemRDD
+        .filter(line => line.split("\\|")(10).contains(date))
+        .map(line => {
+          val lines = line.split("\\|")
+          val orderKey = lines(0).toInt
+          val partKey = lines(1).toInt
+          val suppKey = lines(2).toInt
+          val pArray = pBroadcast.value
+          val sArray = sBroadcast.value
+          (orderKey, (pArray(partKey), sArray(suppKey)))
+        })
+        .sortByKey()
+        .take(20)
+        .foreach(p => println(p._1, p._2._1, p._2._2))
+    }
+  }
+}
